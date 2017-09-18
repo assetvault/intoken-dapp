060bb2db0a48bf679eb4e30a1320deff80010382
vilen <jumutc@gmail.com>
2017-09-19 00:16:35 +0200

    fixing compilation

--- src/scoring.sol
+++ src/scoring.sol
@@ -4,15 +4,15 @@ import "ds-stop/stop.sol";
 import "ds-math/math.sol";
 import "./interfaces.sol";
 
-contract TrustScoring is Scoring, DSStop {
+contract TrustScoring is Scoring, DSStop, DSMath {
 	mapping (address => uint) _scores;
 	uint					  _scoreIncrement;
 
 	function TrustScoring() {
-		_score_inc = 10;
+		_scoreIncrement = 10;
 	}
 
-	function getScore(address user, uint score) stoppable returns (uint score) {
+	function getScore(address user) stoppable returns (uint score) {
 		return _scores[user];
 	}
 
@@ -25,11 +25,15 @@ contract TrustScoring is Scoring, DSStop {
 	}
 
 	function scoreDown(address user) returns (bool res) {
-		_scores[user] = min(0, sub(_scores[user], _scoreIncrement))
+		_scores[user] = min(0, sub(_scores[user], _scoreIncrement));
+
+		return true;
 	}
 
 	function scoreUp(address user) returns (bool res) {
-		_scores[user] = min(200, add(_scores[user], _scoreIncrement))
+		_scores[user] = min(200, add(_scores[user], _scoreIncrement));
+
+		return true;
 	}
 
 }
